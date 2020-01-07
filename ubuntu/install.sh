e23c6f94aee285e9770ae46755b7fd7c4de09143
julian <julianhatzky@googlemail.com>
2020-01-07 13:45:18 -0800

    updated install script, now installes powerlevel10k

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,8 +1,3 @@
-if ! [ $(id -u) = 0 ]; then
-   echo "This script must be run as root" 
-   exit 1
-fi
-
 if ! [ "dpkg -s fonts-powerline" ]; 
 then
   echo 'fonts-powerline is not installed yet...will be installed' >&2
@@ -67,21 +62,20 @@ then
   mkdir $HOME/.dotfiles
 fi
 # synchronize home dir with rcup
-rcup -v
+rcup -v -C
 
 # use relative parent path and copy dotfiles 
 script_dir="$(dirname $(dirname $(realpath $0)) )"
 cp "$script_dir"/dotfiles/* ~/.dotfiles/
 
 #set zsh to default
-chsh -s /usr/bin/zsh root 
+sudo chsh -s /usr/bin/zsh root 
 
 vim +PlugInstall +qall
 
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 
 mkdir -p ~/.local/share/fonts
-cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+cd ~/.local/share/fonts && sudo curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
 
-
-
+echo "please run p10k configure after restart of the terminal"
